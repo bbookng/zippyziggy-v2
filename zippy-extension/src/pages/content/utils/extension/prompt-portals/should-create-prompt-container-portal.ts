862df966640d419c4b1d37ead04f41284a11ec3d
June <godan6869@naver.com>
2023-08-16 20:10:33 +0900

    [FE] refactor: shouldCreatePromptContainerPortal 함수 리팩토링(변수화)

--- zippy-extension/src/pages/content/utils/extension/prompt-portals/should-create-prompt-container-portal.ts
+++ zippy-extension/src/pages/content/utils/extension/prompt-portals/should-create-prompt-container-portal.ts
@@ -1,18 +1,15 @@
 // 대상 요소가 포탈을 생성해야 하는지 판단하는 함수
-export const shouldCreatePromptContainerPortal = (
-  targetElement: Element,
-  isNewChatPageRef: React.MutableRefObject<boolean>
-): boolean => {
-  const isNewChatPage = isNewChatPageRef.current;
-
-  return (
-    targetElement.className === 'flex flex-col text-sm dark:bg-gray-800 h-full' ||
-    targetElement.className.includes('relative flex h-full max-w-full flex-1') ||
-    (isNewChatPage && targetElement.className === 'overflow-hidden w-full h-full relative flex') ||
-    (isNewChatPage &&
-      targetElement.className ===
-        'relative h-full w-full transition-width flex flex-col overflow-auto items-stretch flex-1') ||
-    (isNewChatPage && targetElement.className.includes('react-scroll-to-bottom--css')) ||
-    targetElement.id === '__next'
+export const shouldCreatePromptContainerPortal = (targetElement: Element): boolean => {
+  // 첫 번째 조건: 다른 곳에서 처음 진입했을 경우
+  const isInitialEntry = targetElement.className.includes(
+    'flex flex-col text-sm dark:bg-gray-800 h-full'
   );
+  // 두 번째 조건: new chat 버튼을 클릭했을 경우
+  const isNewChatButtonClicked = targetElement.className.includes(
+    'relative h-full w-full transition-width overflow-auto flex-1'
+  );
+  // 세 번째 조건: Next.js root인 경우
+  const isNextJsRoot = targetElement.id === '__next';
+
+  return isInitialEntry || isNewChatButtonClicked || isNextJsRoot;
 };
--- zippy-extension/src/pages/hooks/content/usePromptContainerPortal.ts
+++ zippy-extension/src/pages/hooks/content/usePromptContainerPortal.ts
@@ -1,5 +1,5 @@
 import { ZP_PROMPT_CONTAINER_ID } from '@pages/constants';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { createPromptPortal } from '@pages/content/utils/extension/prompt-portals/create-prompt-portal';
 import { hideEmptyDiv } from '@pages/content/utils/extension/common/hide-empty-div';
 import { shouldCreatePromptContainerPortal } from '@pages/content/utils/extension/prompt-portals/should-create-prompt-container-portal';
@@ -13,38 +13,36 @@ const handleInitialChatPromptList = ($targetElement: HTMLElement) => {
 
 const usePromptListPortal = () => {
   const [portalContainer, setPortalContainer] = useState(null); // 포탈 컨테이너를 저장하는 state
-  const isNewChatPage = useRef<boolean>(!window.location.href.includes('/c/')); // 현재 페이지가 새 채팅 페이지인지 여부를 저장하는 useRef
+  // const isNewChatPage = useRef<boolean>(!window.location.href.includes('/c/')); // 현재 페이지가 새 채팅 페이지인지 여부를 저장하는 useRef
 
   const createPromptPortalCallback = useCallback(() => createPromptPortal(setPortalContainer), []);
 
-  const observeNextElement = useCallback(
-    (isNewChatPageRef: React.MutableRefObject<boolean>) => {
-      const mutationHandler = (mutations) => {
-        for (const mutation of mutations) {
-          const $targetElement = mutation.target as HTMLElement;
-          if (!document.querySelector('h1.text-4xl')) {
-            handleInitialChatPromptList($targetElement);
-          }
-          if (shouldCreatePromptContainerPortal($targetElement, isNewChatPageRef)) {
-            createPromptPortalCallback();
-            return;
-          }
-          hideEmptyDiv($targetElement);
-        }
-      };
-      const observer = new MutationObserver(mutationHandler);
-      observer.observe(document.getElementById('__next'), { subtree: true, childList: true });
+  const observeNextElement = useCallback(() => {
+    const mutationHandler = (mutations) => {
+      for (const mutation of mutations) {
+        const $targetElement = mutation.target as HTMLElement;
 
-      return () => {
-        observer.disconnect();
-      };
-    },
-    [createPromptPortalCallback]
-  );
+        if (!document.querySelector('h1.text-4xl')) {
+          handleInitialChatPromptList($targetElement);
+        }
+        if (shouldCreatePromptContainerPortal($targetElement)) {
+          createPromptPortalCallback();
+          return;
+        }
+        hideEmptyDiv($targetElement);
+      }
+    };
+    const observer = new MutationObserver(mutationHandler);
+    observer.observe(document.getElementById('__next'), { subtree: true, childList: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [createPromptPortalCallback]);
 
   useEffect(() => {
-    return observeNextElement(isNewChatPage);
-  }, [observeNextElement, isNewChatPage]);
+    return observeNextElement();
+  }, [observeNextElement]);
 
   return portalContainer;
 };
